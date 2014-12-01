28ff04324f4607e695c37faedcd942868c144778
Zubiri <zubiri@zubiri.(none)>
2014-12-01 10:37:56 +0100

    seccion

--- app/routes.js
+++ app/routes.js
@@ -46,9 +46,108 @@ module.exports = function(app) {
 	});
 
 	// =====================================
+	// EN CINES ============================
+	// =====================================
+
+	app.get('/cines', function(req, res) {
+
+		var url='http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json?apikey=25uu9nryfxqb5b65umh6mkkr';
+
+		var request = require('request');
+
+		request({url:url, json:"true"}, function (error, response, body, id) {
+
+			if (!error && response.statusCode == 200) {
+
+				console.log(body);
+
+				res.render('cines', {
+					
+					body: body
+					
+				})
+			}
+			else {
+
+				res.render('index');
+
+			}
+
+		});
+		
+	});
+
+	// =====================================
+	// ESTRENOS ============================
+	// =====================================
+
+	app.get('/estrenos', function(req, res) {
+
+		var url='http://api.rottentomatoes.com/api/public/v1.0/lists/movies/opening.json?apikey=25uu9nryfxqb5b65umh6mkkr';
+
+		var request = require('request');
+
+		request({url:url, json:"true"}, function (error, response, body, id) {
+
+			if (!error && response.statusCode == 200) {
+
+				console.log(body);
+
+				res.render('estrenos', {
+					
+					body: body
+					
+				})
+			}
+			else {
+
+				res.render('index');
+
+			}
+
+		});
+		
+	});
+
+	// =====================================
+	// PRÓXIMAMENTE ========================
+	// =====================================
+
+	app.get('/proximamente', function(req, res) {
+
+		var url='http://api.rottentomatoes.com/api/public/v1.0/lists/movies/upcoming.json?apikey=25uu9nryfxqb5b65umh6mkkr';
+
+		var request = require('request');
+
+		request({url:url, json:"true"}, function (error, response, body, id) {
+
+			if (!error && response.statusCode == 200) {
+
+				console.log(body);
+
+				res.render('proximamente', {
+					
+					body: body
+					
+				})
+			}
+			else {
+
+				res.render('index');
+
+			}
+
+		});
+		
+	});
+
+
+
+	// =====================================
 	// INFO DE CADA PELÍCULA ===============
 	// =====================================
 
+	// Cartelera
 	app.get('/hungerGames', function(req, res) {
 
 
@@ -107,5 +206,183 @@ module.exports = function(app) {
 		
 	});
 
+
+
+	// En cines
+	app.get('/madagascar', function(req, res) {
+
+		var url='http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=25uu9nryfxqb5b65umh6mkkr&q=Penguins+Of+Madagascar&page_limit=1';
+
+		var request = require('request');
+
+		request({url:url, json:"true"}, function (error, response, body, id) {
+
+			if (!error && response.statusCode == 200) {
+
+				console.log(body);
+
+				res.render('madagascar', {
+					
+					body: body
+					
+				})
+			}
+			else {
+
+				res.render('index');
+
+			}
+
+		});
+		
+	});
+
+	app.get('/interstellar', function(req, res) {
+
+		var url='http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=25uu9nryfxqb5b65umh6mkkr&q=Interstellar&page_limit=1';
+
+		var request = require('request');
+
+		request({url:url, json:"true"}, function (error, response, body, id) {
+
+			if (!error && response.statusCode == 200) {
+
+				console.log(body);
+
+				res.render('interstellar', {
+					
+					body: body
+					
+				})
+			}
+			else {
+
+				res.render('index');
+
+			}
+
+		});
+		
+	});
+
+
+
+
+	// Estrenos
+	app.get('/pyramid', function(req, res) {
+
+		var url='http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=25uu9nryfxqb5b65umh6mkkr&q=The+Pyramid&page_limit=1';
+
+		var request = require('request');
+
+		request({url:url, json:"true"}, function (error, response, body, id) {
+
+			if (!error && response.statusCode == 200) {
+
+				console.log(body);
+
+				res.render('pyramid', {
+					
+					body: body
+					
+				})
+			}
+			else {
+
+				res.render('index');
+
+			}
+
+		});
+		
+	});
+
+	app.get('/wild', function(req, res) {
+
+		var url='http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=25uu9nryfxqb5b65umh6mkkr&q=Wild&page_limit=1';
+
+		var request = require('request');
+
+		request({url:url, json:"true"}, function (error, response, body, id) {
+
+			if (!error && response.statusCode == 200) {
+
+				console.log(body);
+
+				res.render('wild', {
+					
+					body: body
+					
+				})
+			}
+			else {
+
+				res.render('index');
+
+			}
+
+		});
+		
+	});
+
+
+
+	// Próximamente
+	app.get('/exodus', function(req, res) {
+
+		var url='http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=25uu9nryfxqb5b65umh6mkkr&q=Exodus:+Gods+and+Kings&page_limit=1';
+
+		var request = require('request');
+
+		request({url:url, json:"true"}, function (error, response, body, id) {
+
+			if (!error && response.statusCode == 200) {
+
+				console.log(body);
+
+				res.render('exodus', {
+					
+					body: body
+					
+				})
+			}
+			else {
+
+				res.render('index');
+
+			}
+
+		});
+		
+	});
+
+
+	app.get('/inherentVice', function(req, res) {
+
+		var url='http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=25uu9nryfxqb5b65umh6mkkr&q=Inherent+Vice&page_limit=1';
+
+		var request = require('request');
+
+		request({url:url, json:"true"}, function (error, response, body, id) {
+
+			if (!error && response.statusCode == 200) {
+
+				console.log(body);
+
+				res.render('inherentVice', {
+					
+					body: body
+					
+				})
+			}
+			else {
+
+				res.render('index');
+
+			}
+
+		});
+		
+	});
 
 }
